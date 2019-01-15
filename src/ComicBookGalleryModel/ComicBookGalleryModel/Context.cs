05dfd32b0ebf26b79e86d976bec0bb4e21ea2d19
Aleksandar <saledj07@gmail.com>
2019-01-15 07:03:41 +0100

    Db connection.

---
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Context.cs
@@ -0,0 +1,19 @@
+﻿using ComicBookGalleryModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGalleryModel
+{
+    class Context : DbContext
+    {
+        public Context() : base("ComicBookGallery")
+        {
+
+        }
+        public DbSet<ComicBook> ComicBooks { get; set; }
+    }
+}
